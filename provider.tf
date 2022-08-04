1007e21b27fd4a6ea232d7b4e1a52092052843ea
francesco-mollica <francescomol97@gmail.com>
2022-08-04 10:43:21 +0200

    add provider

--- provider.tf
+++ provider.tf
@@ -1,13 +1,27 @@
 terraform {
+  backend "s3" {
+    encrypt        = false
+    bucket         = "scae-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    key            = ".terraform/terraform.tfstate"
+    region         = "eu-central-1"
+    
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 3.27"
     }
   }
+
+  required_version = ">= 0.14.9"
 }
 
-# Configure the AWS Provider
 provider "aws" {
+  region = var.region
+}
+
+provider "aws" {
+  alias = "us-east-1"
   region = "us-east-1"
 }
