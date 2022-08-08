6001b99fae3f23315d00d7cdb10a049e22bab6b0
francesco-mollica <francescomol97@gmail.com>
2022-08-08 15:35:49 +0200

    destroy

--- instance.tf
+++ instance.tf
@@ -1,39 +1,39 @@
-resource "aws_instance" "web" {
-  count = 2
-  ami           = "ami-05912b6333beaa478"
-  instance_type = "t2.micro"
-  associate_public_ip_address = "false"
-  subnet_id = aws_subnet.private_subnet.id
+# resource "aws_instance" "web" {
+#   count = 2
+#   ami           = "ami-05912b6333beaa478"
+#   instance_type = "t2.micro"
+#   associate_public_ip_address = "false"
+#   subnet_id = aws_subnet.private_subnet.id
   
-  root_block_device {
-    delete_on_termination = true
-    volume_type = "gp2"
-    volume_size = 30
-    // encrpted = false
-    // iops = ...
-    // throughput = 
-  }
+#   root_block_device {
+#     delete_on_termination = true
+#     volume_type = "gp2"
+#     volume_size = 30
+#     // encrpted = false
+#     // iops = ...
+#     // throughput = 
+#   }
 
-  tags = {
-    Name = "ec2-prod-${count.index}"
-  }
-}
+#   tags = {
+#     Name = "ec2-prod-${count.index}"
+#   }
+# }
 
-resource "aws_ebs_volume" "ebs" {
-  count = 2
-  availability_zone = aws_subnet.private_subnet.availability_zone
-  size              = 11
-  type = "gp2"
-  // encrypted = false
-  // throughput = ...
-}
+# resource "aws_ebs_volume" "ebs" {
+#   count = 2
+#   availability_zone = aws_subnet.private_subnet.availability_zone
+#   size              = 11
+#   type = "gp2"
+#   // encrypted = false
+#   // throughput = ...
+# }
 
-resource "aws_volume_attachment" "ebs_att" {
-  count = 2
-  device_name = "/dev/xvdf"
-  volume_id   = aws_ebs_volume.ebs[count.index].id
-  instance_id = aws_instance.web[count.index].id
-}
+# resource "aws_volume_attachment" "ebs_att" {
+#   count = 2
+#   device_name = "/dev/xvdf"
+#   volume_id   = aws_ebs_volume.ebs[count.index].id
+#   instance_id = aws_instance.web[count.index].id
+# }
 
 
 
--- network.tf
+++ network.tf
@@ -1,12 +1,12 @@
-resource "aws_vpc" "prophet" {
-  cidr_block = "10.0.0.0/16"
-}
+# resource "aws_vpc" "prophet" {
+#   cidr_block = "10.0.0.0/16"
+# }
 
-resource "aws_subnet" "private_subnet" {
-  vpc_id     = aws_vpc.prophet.id
-  cidr_block = "10.0.0.0/24"
+# resource "aws_subnet" "private_subnet" {
+#   vpc_id     = aws_vpc.prophet.id
+#   cidr_block = "10.0.0.0/24"
 
-  tags = {
-    Name = "private_subnet"
-  }
-}
+#   tags = {
+#     Name = "private_subnet"
+#   }
+# }
