85829d3feb10bcc4336b49124c71782fd7f1716e
francesco-mollica <44461898+francesco-mollica@users.noreply.github.com>
2022-08-08 13:07:10 +0200

    update instance.tf
    aaaa

--- instance.tf
+++ instance.tf
@@ -2,11 +2,11 @@ resource "aws_instance" "web" {
   count = 2
   ami           = "ami-05912b6333beaa478"
   instance_type = "t2.micro"
-  associate_public_ip_address = false
+  associate_public_ip_address = "false"
   
 
   tags = {
-    Name = "ec2-prod-{$count.index}"
+    Name = "ec2-prod-${count.index}"
   }
 }
 
