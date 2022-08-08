a818a05e86bf615bf47599e3166256f439699d79
francesco-mollica <francescomol97@gmail.com>
2022-08-08 14:08:44 +0200

    aaa

--- network.tf
+++ network.tf
@@ -3,7 +3,7 @@ resource "aws_vpc" "prophet" {
 }
 
 resource "aws_subnet" "private_subnet" {
-  vpc_id     = aws_vpc.prophet
+  vpc_id     = aws_vpc.prophet.id
   cidr_block = "10.0.0.0/24"
 
   tags = {
