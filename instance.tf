2ca2aa31dc286990fb9072b257a7f0f5d9c83f72
francesco-mollica <44461898+francesco-mollica@users.noreply.github.com>
2022-08-08 13:01:30 +0200

    Update instance.tf
    change ami id

--- instance.tf
+++ instance.tf
@@ -1,9 +1,12 @@
 resource "aws_instance" "web" {
   count = 2
-  ami           = "ami-090fa75af13c156b4"
+  ami           = "ami-05912b6333beaa478"
   instance_type = "t2.micro"
+  associate_public_ip_address = false
+  
 
   tags = {
-    Name = "ec2-prod"
+    Name = "ec2-prod-{$count.index}"
   }
 }
+
